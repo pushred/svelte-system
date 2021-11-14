5bc88c7fd7decd5768667129ad1ad14729985869
Eric Casthart <eric@pushred.co>
2021-11-14 09:35:52 -0500

    Refactor meow → sade
    * Align with SvelteKit
    * Prepare for commands (meow doesn't really help with that)

--- packages/generator/lib/cli.js
+++ packages/generator/lib/cli.js
@@ -4,47 +4,44 @@ import { relative, resolve } from 'path'
 
 import chalk from 'chalk'
 import { cosmiconfigSync } from 'cosmiconfig'
-import meow from 'meow'
+import sade from 'sade'
 
 import { generateComponents } from './generate.js'
 
-const HELP = `
-  Usage
-    $ svelte-system-components
+const cli = sade('svelte-sytem', true)
 
-  Options
-    --config, -c  Path to config file
-    --output, -o  Path to output generated components
-`
+cli
+  .option('-c --config', 'Path to config file')
+  .option('-o --output', 'Path to output generated components')
 
-const cli = meow(HELP, {
-  importMeta: import.meta,
+cli.action((options) => {
+  const explorer = cosmiconfigSync('svelte-system')
+
+  const explorerResult = options.config
+    ? explorer.load(options.config)
+    : explorer.search()
+
+  if (explorerResult === null) {
+    throw new Error(
+      'Could not find required configuration, see README for details'
+    )
+  }
+
+  const userConfig = explorerResult.config
+  const outputPath = options.output || userConfig.outputPath
+
+  const components = generateComponents({
+    outputPath,
+    theme: userConfig.theme,
+  })
+
+  const relativeOutputPath = relative(resolve('..'), outputPath)
+
+  console.log(
+    chalk.green(
+      `✔ ${components.length} components generated and saved to ${relativeOutputPath}`
+    )
+  )
 })
 
-const explorer = cosmiconfigSync('svelte-system')
-
-const explorerResult = cli.flags.config
-  ? explorer.load(cli.flags.config)
-  : explorer.search()
-
-if (explorerResult === null) {
-  throw new Error(
-    'Could not find required configuration, see README for details'
-  )
-}
-
-const userConfig = explorerResult.config
-const outputPath = cli.flags.output || userConfig.outputPath
-
-const components = generateComponents({
-  outputPath,
-  theme: userConfig.theme,
-})
-
-const relativeOutputPath = relative(resolve('..'), outputPath)
-
-console.log(
-  chalk.green(
-    `✔ ${components.length} components generated and saved to ${relativeOutputPath}`
-  )
-)
+cli.parse(process.argv)
